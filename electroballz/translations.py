be3e5343752ce2409eb829d5100198bae1e59532
Marek Nečada <marek.necada@aalto.fi>
2014-05-26 12:07:41 +0300

    C, D, B translation coefficients

--- electroballz/translations.py
+++ electroballz/translations.py
@@ -1,4 +1,6 @@
-import clebsch from qutip
+from qutip import clebsch
+# N.B. toto může být pomalejší než pokaždé volat scipy.special.lpmv, zkontrolovat!
+from scipy.special import lpmv
 
 #TODO check data types, optimize
 #!!!! Je clebsch ta správná funkce pro 3j symbol?
@@ -19,23 +21,61 @@ def factorial_frac(numer, denom):
     return numer / float(denom)
 
 
-def bks_C(m, l, mp, lp, d, eta, z):
+def bks_C(m, l, mp, lp, k, d, eta, z):
     """[BKS 1991], (5.36)"""
     coseta = cos(eta)
-    res = 0
+    res = 0.
     for lambd in range(abs(l-lp) + 1, l + lp + 1):
-        # If l + l' + λ is od, the first 3j symbol is zero.
+        # If l + l' + λ is odd, the first 3j symbol is zero.
         if 0 == (l + lp + lambd) % 2:
             line2 = (-1)**(lp - l + lambd) * (2*lambd + 1) * sqrt(
-                    #TODO podíl faktorů udělat nějak chytřeji. Co dělá python s přetečením?
+                    #TODO podíl faktoriálů udělat nějak chytřeji. Co dělá python s přetečením?
                     (2*l+1)*(2*lp+1)*factorial_frac(lambd-m+mp,lambd+m-mp) /
                     (l * (l+1) * lp * (lp + 1)))
             line3 = wigner3j(l, lp, lambd, 0, 0, 0)
             line3 *= wigner3j(l, lp, lambd, m, -mp, mp - m)
             line3 *= l*(l+1) + lp*(lp+1) - lambd*(lambd+1)
+            # !!! Bacha na z, to pak přijde kompletně předělat
+            line4 = z(lambd, k*d) * lpmv(m - mp, lambd, coseta)
+            res += line2 * line3 * line4
+    res *= 0.25 * (-1)**(m+mp) * sqrt((2-(m==0))*(2-(mp==0)))
+    return res
+
+def bks_D(m, l, mp, lp, k, d, eta, z):
+    """[BKS 1991], (5.37)"""
+    coseta  = cos(eta)
+    res = 0.
+    for lambd in range(abs(l-lp)+1, l+lp):
+        if ((l + lp + lambd) % 2):
+            line2 = (1j)**(lp - l + lambd + 1) * (2*lambd + 1) * sqrt(
+                    (2*l+1)*(2*lp+1)*factorial_frac(lambd-m+mp,lambd+m-mp) /
+                    (l * (l+1) * lp * (lp + 1))))
+            line3 = wigner3j(l, lp, lambd-1, 0, 0, 0)
+            line3 *= wigner3j(l, lp, lambd, m, -mp, mp - m)
+            line3 *= sqrt(lambd**2 - (l-lp)**2)
+            # TODO až to bude spolehlivě fungovat, přeuspořádat.
+            line4 = sqrt((l+lp+1)**2 - lambd**2)
+            line4 *= z(lambd, k*d) * lpmv(m - mp, lambd, coseta)
+            res += line2 * line3 * line4
+    res *= 0.25 * (-1)**(m+mp) * sqrt((2-(m==0))*(2-(mp==0)))
+    return res
+
+def bks_B(m, l, mp, lp, k, d, eta, z):
+    """[BKS 1991], (5.38)"""
+    coseta = cos(eta)
+    res = 0.
+    for lambd in range(abs(l-lp) + 1, l + lp + 1):
+        if 0 == (l + lp + lambd) % 2:
+            line2 = (-1)**(lp - l + lambd) * (2*lambd + 1) * sqrt(
+                    (2*l+1)*(2*lp+1)*factorial_frac(lambd-m+mp,lambd+m-mp))
+            line3 = wigner3j(l, lp, lambd, 0, 0, 0)
+            line3 *= wigner3j(l, lp, lambd, m, -mp, mp-m)
+            line3 *= z(k*d) * lpmv(m - mp, lambd, coseta)
+            res += line2 * line3
+    res *= 0.5 * (-1)**(m+mp) * sqrt((2-(m==0)) * (2-(mp==0)))
+    return res
 
 
-             
             
 
     
