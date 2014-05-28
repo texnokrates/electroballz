f58889c8379b63eb1f627e81ec8e3f8a36069d54
Marek Nečada <marek@necada.org>
2014-05-28 09:29:13 +0000

    S matrix elements.

--- electroballz/translations.py
+++ electroballz/translations.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from qutip import clebsch
+from math import sin, cos
 # N.B. toto může být pomalejší než pokaždé volat scipy.special.lpmv, zkontrolovat!
 from scipy.special import lpmv
 
@@ -49,7 +51,7 @@ def bks_D(m, l, mp, lp, k, d, eta, z):
         if ((l + lp + lambd) % 2):
             line2 = (1j)**(lp - l + lambd + 1) * (2*lambd + 1) * sqrt(
                     (2*l+1)*(2*lp+1)*factorial_frac(lambd-m+mp,lambd+m-mp) /
-                    (l * (l+1) * lp * (lp + 1))))
+                    (l * (l+1) * lp * (lp + 1)))
             line3 = wigner3j(l, lp, lambd-1, 0, 0, 0)
             line3 *= wigner3j(l, lp, lambd, m, -mp, mp - m)
             line3 *= sqrt(lambd**2 - (l-lp)**2)
@@ -75,8 +77,52 @@ def bks_B(m, l, mp, lp, k, d, eta, z):
     res *= 0.5 * (-1)**(m+mp) * sqrt((2-(m==0)) * (2-(mp==0)))
     return res
 
-
-            
-
-    
+def translation_S_real(tau, sigma, m, l, d, taup, sigmap, mp, lp, eta, psi, z, k):
+    """The S matrix elements from [BKS 1991], (5.29-5.35) for real spherial harmonics"""
+    if (sigma == 'e'):
+        sigma = 0
+    if (sigma == 'o'):
+        sigma = 1
+    # longitudinal and transversal modes do not couple
+    if tau == 3 and taup != 3:
+        return 0
+    if taup == 3 and tau != 3:
+        return 0
+    if tau == 2:
+        # [BKS 1991, (5.33)]
+        taup = 3 - taup
+        tau = 1
+    if tau == 1:
+        if taup == 1:
+            if (sigma == sigmap):
+                # [BKS 1991], (5.29)
+                return ((-1)**mp * bks_C(m,l,mp,lp,k,d,eta,z) * cos((m-mp)*psi)
+                        + (-1)**sigma * bks_C(m,l,-mp,lp,k,d,eta,z) *
+                        cos((m+mp)*psi))
+            else:
+                # [BKS 1991], (5.30)
+                return ((-1)**(mp+sigmap) * bks_C(m,l,mp,lp,k,d,eta,z) *
+                        sin((m-mp)*psi) 
+                        + bks_C(m,l,-mp,lp,k,d,eta,z) * sin((m+mp)*psi))
+        if taup == 2:
+            if (sigma == sigmap):
+                # [BKS 1991], (5.32)
+                return ((-1)**mp * bks_D(m,l,mp,lp,k,d,eta,z) * sin((m-mp)*psi)
+                        + (-1)**sigma * bks_D(m,l,-mp,lp,k,d,eta,z) *
+                        sin((m+mp)*psi))
+            else:
+                # [BKS 1991], (5.31)
+                return ((-1)**(mp+sigma) * bks_D(m,l,mp,lp,k,d,eta,z) *
+                        cos((m-mp)*psi)
+                        - bks_D(m,l,-mp,lp,k,d,eta,z) * cos((m+mp)*psi))
+        if taup == 3:
+            if (sigma == sigmap):
+                # [BKS 1991, (5.34)
+                return ((-1)**mp * bks_B(m,l,mp,lp,k,d,eta,z) * cos((m-mp)*psi)
+                        + (-1)**sigma * bks_B(m,l,-mp,lp,k,d,eta,z) 
+                        * cos((m+mp)*psi))
+            else:
+                return ((-1)**(mp+sigmap) * bks_B(m,l,-mp,lp,k,d,eta,z)
+                        * sin((m-mp)*psi)
+                        + bks_B(m,l,-mp,lp,k,d,eta,z) * sin((m+mp)*psi))
 
